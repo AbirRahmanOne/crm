c3a3ca124178e2e9a3218dea5dbbdae8501ba566
AbirRahmanOne <abir.talks@gmail.com>
2021-01-24 19:53:32 +0600

    Inline formset

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,3 +6,9 @@ class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
